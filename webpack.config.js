1a7bf73c6e1c647929e8f56d1deb555aeada718d
maggie.ng <iamtaiching@gmail.com>
2017-01-02 16:23:46 -0800

    clean up after eslint run

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+let debug = process.env.NODE_ENV !== 'production';
+let webpack = require('webpack');
+let path = require('path');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  devtool: debug ? "inline-source-map" : null,
-  entry: "./js/client.js",
+  devtool: debug ? 'inline-source-map' : null,
+  entry: './js/client.js',
   module: {
     loaders: [
       {
@@ -14,17 +14,17 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'latest'],
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "client.min.js",
+    filename: 'client.min.js',
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
   ],
 };
