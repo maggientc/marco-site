1652ee4407d770d83b2be3d20b770b885bf8066f
maggie.ng <iamtaiching@gmail.com>
2017-04-02 18:08:59 -0700

    Added new page - about

---
+++ src/js/pages/About/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import IconItem from '../../components/IconItem';
+import './about.scss';
+
+/** Home component */
+class About extends React.Component {
+  /** @param {obj} props from parent. */
+  constructor(props) {
+    super(props);
+  }
+
+  /** @return {html} About */
+  render() {
+    const labels = this.props.labels;
+    const iconList = [{
+      icon: 'desktop',
+      title: labels.responsiveTitle,
+      contact: labels.responsiveContact,
+    }, {
+      icon: 'heart',
+      title: labels.passionTitle,
+      contact: labels.passionContact,
+    }, {
+      icon: 'diamond',
+      title: labels.designTitle,
+      contact: labels.designContact,
+    }, {
+      icon: 'cog',
+      title: labels.supportTitle,
+      contact: labels.supportContact,
+    }];
+
+    return (
+      <div className='container' id='about'>
+        <h3 className='container-title'>{labels.title}</h3>
+        <p className='container-subTitle'>{labels.subTitle}</p>
+        <div className='iconList'>
+          { iconList.map((item) => <IconItem {...item} />) }
+        </div>
+      </div>
+    );
+  }
+}
+
+About.propTypes = {
+  labels: React.PropTypes.object,
+};
+
+export default About;
